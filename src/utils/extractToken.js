cc1b750e42cac6aea752d31de947fadebf279eb3
DeTexTeR-develop <ujjwalsingh90official@gmail.com>
2026-04-29 14:58:04 +0530

    added support for extracting auth from cookies

--- src/utils/extractToken.js
+++ src/utils/extractToken.js
@@ -1,7 +1,16 @@
 const extractToken = (req) => {
-  const header = req.headers.authorization;
-  if (!header) return null;
-  return header.split(" ")[1];
+    // 1. Check cookies first
+    if (req.cookies?.token) {
+        return req.cookies.token;
+    }
+
+    // 2. Check Authorization header
+    const header = req.headers.authorization;
+    if (header && header.startsWith("Bearer ")) {
+        return header.split(" ")[1];
+    }
+
+    return null;
 };
 
 module.exports = extractToken;
